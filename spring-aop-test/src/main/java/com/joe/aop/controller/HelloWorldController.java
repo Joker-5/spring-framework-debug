41155f574a3c63fbf9dd77bd1a3d96430c2f9998
john_doe <2725284321@qq.com>
2022-11-26 23:11:22 +0800

    feat: @Lookup 注解实现原理

--- spring-aop-test/src/main/java/com/joe/aop/controller/HelloWorldController.java
+++ spring-aop-test/src/main/java/com/joe/aop/controller/HelloWorldController.java
@@ -3,30 +3,27 @@ package com.joe.aop.controller;
 import com.joe.aop.service.ElectricService;
 import com.joe.aop.service.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * Created by joker on 2022/11/24.
  */
+
 @RestController
 public class HelloWorldController {
-	@Autowired
-	private ElectricService electricService;
-	@Autowired
-	private ServiceImpl serviceImpl;
 
-	@GetMapping(path = "/charge")
-	public void charge() throws Exception {
-		electricService.payWithAliPay();
-	}
-
-	@GetMapping(path = "/charge1")
-	public void charge1() {
-	}
-
-	@GetMapping(path = "/hi")
+	@RequestMapping(path = "hi", method = RequestMethod.GET)
 	public String hi() {
-		return "helloworld, service is : " + serviceImpl;
+		return "helloworld, service is : " + getServiceImpl();
 	}
-	
+
+	@Lookup
+	public ServiceImpl getServiceImpl() {
+		return null;
+	}
+
 }
